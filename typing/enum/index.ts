d242b1918fc173bc259d70f2b1ffc1f9cbb51c40
Yab1 <yabilisanu@gmail.com>
2024-01-05 14:01:08 +0300

    Add enum exports to index file for easier access
    This change adds exports for various enum types to the index file,
    facilitating easier access to the enums throughout the codebase.
    
    #ISSUE-123

---
+++ typing/enum/index.ts
@@ -0,0 +1,7 @@
+export { default as ActionEnum } from "./ActionEnum";
+export * from "./StatusEnum";
+export { default as TargetEnum } from "./TargetEnum";
+export { default as ToggleableEnum } from "./ToggleableEnum";
+export { default as UserRoleEnum } from "./UserRoleEnum";
+export { default as SettingEnum } from "./SettingEnum";
+export { default as SocialsEnum } from "./SocialsEnum";
