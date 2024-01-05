9d02b01ebd2894e9d1a55ee4ee0b8b290cb3e6a8
Yab1 <yabilisanu@gmail.com>
2024-01-05 14:03:41 +0300

    Create user data file with sample user information
    Added a new file to store user data including user ID, name, avatar
    image, and about text. This will facilitate easy access to user
    information throughout the application. The sample user data will serve
    as placeholders during development.
    
    #ISSUE-123

---
+++ data/user-data.ts
@@ -0,0 +1,13 @@
+import { UserArray } from "@/typing";
+
+const userData: UserArray = [
+  {
+    id: "1",
+    name: "Yeabsera Lisanework",
+    avater:
+      "https://media.licdn.com/dms/image/D5603AQHlyfMGnKvIPA/profile-displayphoto-shrink_200_200/0/1693337536934?e=1703116800&v=beta&t=VCnGeR2CMoyMLURj3UYZq0jTJY6jpWvnxtAh1ZRz6ms",
+    about: "Software Engineer",
+  },
+];
+
+export default userData;
