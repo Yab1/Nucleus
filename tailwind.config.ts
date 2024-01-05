555703dc3427b3bed22a0906663bdad5e163856c
Yab1 <yabilisanu@gmail.com>
2024-01-05 13:57:08 +0300

    Refactor Tailwind config to add color and content paths
    Adjust the Tailwind configuration to include a new color and update
    content paths to include a specific module. This change also adds a new
    plugin. Improves theme customization and extends content selection. No
    issues fixed.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,19 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "grayish-black": "#1b1c1e",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("flowbite/plugin")],
+};
+export default config;
