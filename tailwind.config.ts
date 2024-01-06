1319b39c7fec08587265dca4965d8084f801c69b
Yab1 <yabilisanu@gmail.com>
2024-01-06 06:32:34 +0300

    Refactor Tailwind config to include Material Tailwind styles
    Replace Tailwind CSS configuration with Material Tailwind utility for
    enhanced styles and components. This change adds support for new color
    definitions. No issues fixed.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,19 @@
-import type { Config } from 'tailwindcss'
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+module.exports = withMT({
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "grayish-black": "#1b1c1e",
+        "misty-sky": "#f6f7ff",
+        silver: "#e5e5e5",
       },
     },
   },
   plugins: [],
-}
-export default config
+});
