824492ab1d30154215dc19f754557f26f9a84b23
Yab1 <yabilisanu@gmail.com>
2024-01-06 19:58:46 +0300

    Refactor Awsomebar layout for improved search UI
    Reorganized Awsomebar layout to enhance search user interface, with
    updated styling and UI components to provide a more visually appealing
    and user-friendly search experience. No related issues.

--- components/widgets/Awsomebar.tsx
+++ components/widgets/Awsomebar.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { Input } from "@material-tailwind/react";
+import { CiSearch } from "react-icons/ci";
+
 export default function Awsomebar() {
   return (
-    <div>
-      <h1>Awsomebar</h1>
+    <div className="w-96 flex place-items-center">
+      <div className="bg-gray-200 p-[7.8px] rounded-md rounded-e-none">
+        <CiSearch size={25} color="gray" />
+      </div>
+      <Input
+        crossOrigin={undefined}
+        type="text"
+        placeholder="Search ..."
+        className="!bg-gray-200 border-none !rounded-s-none"
+        labelProps={{
+          className: "hidden",
+        }}
+        containerProps={{ className: "min-w-[100px]" }}
+      />
     </div>
   );
 }
