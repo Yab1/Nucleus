ef4e7b2c1f1fdcd4dd1687a261ee8f5285191e50
Yab1 <yabilisanu@gmail.com>
2024-01-06 19:58:32 +0300

    Adjust topbar layout gap for better spacing
    Increase the gap between items in the topbar for improved visual spacing
    and alignment. This change aims to enhance the user interface by
    providing better visual separation between the components. No related
    issues.

--- components/layouts/Topbar.tsx
+++ components/layouts/Topbar.tsx
@@ -2,7 +2,7 @@ import { Awsomebar, Notifications, UserMenu } from "@/components/widgets";
 
 export default function Topbar() {
   return (
-    <header className="h-20 px-10 py-5 flex place-items-center gap-5">
+    <header className="h-20 px-10 py-5 flex place-items-center gap-8">
       <Awsomebar />
       <Notifications />
       <UserMenu />
