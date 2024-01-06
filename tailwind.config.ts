8e4bdeb887286454732e263b1cde23febe9c5063
Yab1 <yabilisanu@gmail.com>
2024-01-06 19:58:24 +0300

    Add light and steel-gray colors to the tailwind configuration This change adds two new colors, "gray-light" and "steel-gray", to the tailwind configuration file to expand the available color palette. This expands the design options for the UI components using Tailwind CSS. It improves the design flexibility and consistency. No associated issues.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,9 @@ module.exports = withMT({
       colors: {
         "grayish-black": "#1b1c1e",
         "misty-sky": "#f6f7ff",
+        "gray-light": "#a5a5a5",
         silver: "#e5e5e5",
+        "steel-gray": "#757575",
       },
     },
   },
