80d7e064fd608205773ddfc35912adb9e868d65e
Yab1 <yabilisanu@gmail.com>
2024-01-12 21:10:48 +0300

    Refactor route structure to accommodate new layout configuration
    Reorganize route structure to facilitate integration with a new layout
    configuration, enhancing modularity and scalability. Introduce new
    layout-related types for improved code clarity. No known issues. No
    issue references.

--- routes.ts
+++ routes.ts
@@ -1,3 +1,4 @@
+import { LayoutArray, RouteArray } from "@/typing";
 import { FaRegClock, FaUserCircle } from "react-icons/fa";
 import { TiFlashOutline } from "react-icons/ti";
 import { LuCalendar } from "react-icons/lu";
@@ -7,43 +8,127 @@ import { CiGrid41 } from "react-icons/ci";
 import { RiInbox2Fill } from "react-icons/ri";
 import { AiFillSetting } from "react-icons/ai";
 import { BiSolidHelpCircle } from "react-icons/bi";
-import { RouteArray } from "@/typing";
+import { MdOutlineSecurity, MdAttachMoney } from "react-icons/md";
+import { BsWallet2 } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
+import { VscSymbolColor, VscGraph } from "react-icons/vsc";
 
-export const primaryRoutes: RouteArray = [
+export const primaryRoutes: LayoutArray = [
   {
-    name: "Tasks",
-    path: "/tasks",
-    icon: FaRegClock,
+    title: "General",
+    routes: [
+      {
+        routeTitle: "Dashboard",
+        routePath: "/",
+        routeIcon: CiGrid41,
+      },
+      {
+        routeTitle: "Inbox",
+        routePath: "/inbox",
+        routeIcon: RiInbox2Fill,
+      },
+    ],
   },
-  { name: "Activities", path: "/activities", icon: TiFlashOutline },
+  {
+    title: "Features",
+    routes: [
+      {
+        routeTitle: "Projects",
+        routePath: "/projects",
+        routeIcon: GoProject,
+      },
+      {
+        routeTitle: "My Tasks",
+        routePath: "/tasks",
+        routeIcon: FaRegClock,
+      },
+      {
+        routeTitle: "Team members",
+        routePath: "/team",
+        routeIcon: SlPeople,
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    routes: [
+      {
+        routeTitle: "Calendar",
+        routePath: "/calendar",
+        routeIcon: LuCalendar,
+      },
+      {
+        routeTitle: "Report",
+        routePath: "/report",
+        routeIcon: VscGraph,
+      },
+    ],
+  },
 ];
+// { name: "Activities", path: "/activities", icon: TiFlashOutline },
+// export const primaryRoutes: RouteArray = [
+//   {
+//     name: "Tasks",
+//     path: "/tasks",
+//     icon: FaRegClock,
+//   },
+//   { name: "Activities", path: "/activities", icon: TiFlashOutline },
+// ];
 
-export const secondaryRoutes: RouteArray = [
-  { name: "Dashboard", path: "/", icon: CiGrid41 },
-  { name: "Projects", path: "/projects", icon: GoProject },
-  { name: "Teams", path: "/teams", icon: SlPeople },
-  { name: "Calendar", path: "/calendar", icon: LuCalendar },
-];
+// export const secondaryRoutes: RouteArray = [
+//   { name: "Dashboard", path: "/", icon: CiGrid41 },
+//   { name: "Projects", path: "/projects", icon: GoProject },
+//   { name: "Teams", path: "/teams", icon: SlPeople },
+//   { name: "Calendar", path: "/calendar", icon: LuCalendar },
+// ];
 
-export const tertiaryRoutes: RouteArray = [
-  {
-    name: "my profile",
-    path: "/profile",
-    icon: FaUserCircle,
-  },
-  {
-    name: "inbox",
-    path: "/inbox",
-    icon: RiInbox2Fill,
-  },
-  {
-    name: "setting",
-    path: "/setting",
-    icon: AiFillSetting,
-  },
-  {
-    name: "help",
-    path: "/help",
-    icon: BiSolidHelpCircle,
-  },
-];
+// export const tertiaryRoutes: RouteArray = [
+//   {
+//     name: "my profile",
+//     path: "/profile",
+//     icon: FaUserCircle,
+//   },
+//   {
+//     name: "inbox",
+//     path: "/inbox",
+//     icon: RiInbox2Fill,
+//   },
+//   {
+//     name: "settings",
+//     path: "/settings",
+//     icon: AiFillSetting,
+//   },
+//   {
+//     name: "help",
+//     path: "/help",
+//     icon: BiSolidHelpCircle,
+//   },
+// ];
+
+// export const settingRoutes: RouteArray = [
+//   {
+//     name: "Personal Details",
+//     path: "/settings/profile",
+//     icon: CgProfile,
+//   },
+//   {
+//     name: "Security",
+//     path: "/settings/security",
+//     icon: MdOutlineSecurity,
+//   },
+//   {
+//     name: "Plans & Pricing",
+//     path: "/settings/plans & pricing",
+//     icon: BsWallet2,
+//   },
+//   {
+//     name: "Payment & Billing",
+//     path: "/settings/plans & pricing",
+//     icon: MdAttachMoney,
+//   },
+//   {
+//     name: "Themes",
+//     path: "/settings/themes",
+//     icon: VscSymbolColor,
+//   },
+// ];
