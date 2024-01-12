d96bcbef57646b65a13368e11c6b5b03520c8476
Yab1 <yabilisanu@gmail.com>
2024-01-12 21:50:22 +0300

    Refactor Topbar layout for improved user experience
    Restructured Topbar layout to prioritize essential actions and display
    additional user settings, enhancing user interaction. Issue #123

--- components/layouts/Topbar.tsx
+++ components/layouts/Topbar.tsx
@@ -1,10 +1,26 @@
-import { Notifications, UserMenu } from "@/components/widgets";
+"use client";
+
+import { Notifications, Settings, UserMenu } from "@/components/widgets";
+import { Typography } from "@material-tailwind/react";
 
 export default function Topbar() {
   return (
-    <header className="h-20 px-10 py-5 flex place-items-center gap-8 border-b border-grayish-blue">
-      <Notifications />
-      <UserMenu />
+    <header className="h-20 px-10 py-5 flex place-items-center justify-between border-b border-grayish-blue">
+      <div>
+        <Typography placeholder={undefined} variant="h5">
+          Getting Started
+        </Typography>
+        <Typography placeholder={undefined} variant="small" color="gray">
+          Take a few minutes to discover about new features!
+        </Typography>
+      </div>
+
+      <div className="flex gap-4 place-items-center">
+        <Notifications />
+        <Settings />
+        <span className="h-14 w-px bg-grayish-blue"></span>
+        <UserMenu />
+      </div>
     </header>
   );
 }
