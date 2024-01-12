38dd96efa2492e61c720cda4254911769ca3f302
Yab1 <yabilisanu@gmail.com>
2024-01-12 21:11:48 +0300

    Refactor Topbar component layout and remove Awsomebar
    Removed Awsomebar from the Topbar component layout to simplify the
    structure and added a bottom border for visual clarity. This change
    enhances the component's clarity and removes unnecessary visual
    elements. Fixes #123.

--- components/layouts/Topbar.tsx
+++ components/layouts/Topbar.tsx
@@ -1,9 +1,8 @@
-import { Awsomebar, Notifications, UserMenu } from "@/components/widgets";
+import { Notifications, UserMenu } from "@/components/widgets";
 
 export default function Topbar() {
   return (
-    <header className="h-20 px-10 py-5 flex place-items-center gap-8">
-      <Awsomebar />
+    <header className="h-20 px-10 py-5 flex place-items-center gap-8 border-b border-grayish-blue">
       <Notifications />
       <UserMenu />
     </header>
