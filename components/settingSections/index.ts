34341da3288b5cbcea294b3e720872d4d1abd5dc
Yab1 <yabilisanu@gmail.com>
2024-01-13 12:04:54 +0300

    Add setting sections exports
    This commit adds exports for setting section components to allow for
    their usage elsewhere in the codebase. This will enable easy access to
    setting section components and improve code organization. No relevant
    issue references.

---
+++ components/settingSections/index.ts
@@ -0,0 +1,5 @@
+export { default as EditProfile } from "./EditProfile";
+export { default as Themes } from "./Themes";
+export { default as Personal } from "./Personal";
+export { default as Workspace } from "./Workspace";
+export { default as Socials } from "./Socials";
