f325b4ee640b3d67850f0377db558a6387b9fb36
Yab1 <yabilisanu@gmail.com>
2024-01-13 17:25:10 +0300

    Add new color "black-pearl" to tailwind.config This change introduces the "black-pearl" color to the tailwind.config for use in styling components. It enriches the available color palette, providing more flexibility in design choices. This aligns with the goal of enhancing the visual presentation of the application. No further issues referenced.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ module.exports = withMT({
       colors: {
         "grayish-blue": "#e1e3e4",
         "off-white": "#f5f4f4",
+        "black-pearl": "#1c1c1c",
+
         "grayish-black": "#1b1c1e",
         "misty-sky": "#f6f7ff",
         "gray-light": "#a5a5a5",
