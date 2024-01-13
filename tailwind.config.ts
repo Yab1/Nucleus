bbdebee86df20c3073ce936c383a142ff05039aa
Yab1 <yabilisanu@gmail.com>
2024-01-13 20:03:29 +0300

    Refactor color definitions for light and dark themes
    Consolidated and organized color definitions for better theme
    management, aesthetic consistency, and ease of customization. No major
    impact expected. Refs #123, #456.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,46 @@ module.exports = withMT({
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        light: {
+          primary: "#f5f4f4",
+          secondary: "",
+        },
+        dark: {
+          primary: "#1c1c1c",
+          secondary: "#232323",
+          tertiary: "#292929",
+        },
+      },
+      textColor: {
+        light: {
+          primary: "",
+          secondary: "#4d4764",
+        },
+        dark: {
+          primary: "#bdbdbd",
+          secondary: "#d2d2d2",
+          tertiary: "#acacac",
+          // tertiary: "#cccccc",
+          // fourth: "#a1a1a1",
+        },
+      },
+      borderColor: {
+        light: {
+          primary: "#e1e3e4",
+          secondary: "",
+        },
+        dark: {
+          primary: "#383838",
+          secondary: "",
+        },
+      },
       colors: {
-        "grayish-blue": "#e1e3e4",
-        "off-white": "#f5f4f4",
+        "border-light-primary": "#e1e3e4", // primary light border
+        "light-primary": "#f5f4f4",
         "black-pearl": "#1c1c1c",
+        charcoal: "#232323",
+        gunmetal: "#383838",
 
         "grayish-black": "#1b1c1e",
         "misty-sky": "#f6f7ff",
