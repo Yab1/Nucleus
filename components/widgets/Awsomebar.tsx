4739400f570614ced8f147da55584dc3889595f7
Yab1 <yabilisanu@gmail.com>
2024-01-13 20:02:12 +0300

    Refactor dark mode styling for Awsomebar button
    Utilize dark mode state to dynamically set button color, optimizing user
    experience. #123

--- components/widgets/Awsomebar.tsx
+++ components/widgets/Awsomebar.tsx
@@ -1,18 +1,21 @@
 "use client";
 
+import { RootState } from "@/redux/store";
 import { Button } from "@material-tailwind/react";
 import { CiSearch } from "react-icons/ci";
+import { useSelector } from "react-redux";
 
 export default function Awsomebar() {
+  const darkMode = useSelector((state: RootState) => state.ui.darkMode);
+
   return (
     <Button
       placeholder={undefined}
       variant="filled"
       size="sm"
-      color="gray"
-      className="flex items-center gap-3 mx-3 border border-grayish-blue !bg-off-white normal-case font-normal text-black shadow-none rounded-md"
+      className="flex items-center gap-3 mx-3 border border-light-primary !bg-light-primary normal-case font-normal shadow-none rounded-md dark:!bg-dark-secondary dark:border-dark-primary text-light-secondary dark:text-dark-secondary"
     >
-      <CiSearch size={20} color="black" />
+      <CiSearch size={20} color={darkMode ? "#d2d2d2" : "#4d4764"} />
       Search
     </Button>
   );
