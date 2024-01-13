b6188d977ea8fcd7459375b2138af52553fa0a8e
Yab1 <yabilisanu@gmail.com>
2024-01-13 10:16:54 +0300

    Refactor Sidebar to Dynamically Render Primary Routes
    Update Sidebar to dynamically render primary routes using a map
    function, enhancing maintainability and scalability. Improve user
    experience by providing a more flexible navigation structure. #134

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { Awsomebar } from "../widgets";
+import { primaryRoutes } from "@/routes";
+import { Typography } from "@material-tailwind/react";
 
 export default function Sidebar() {
   return (
@@ -13,8 +17,46 @@ export default function Sidebar() {
         Nucleus
       </Link>
 
-      <div className="flex flex-col gap-5 mt-5">
+      <div className="flex flex-col gap-5 my-5">
         <Awsomebar />
+      </div>
+
+      <div className="flex flex-col ml-6 gap-5">
+        {primaryRoutes.map(({ title, routes }) => (
+          <div key={title}>
+            <Typography
+              placeholder={undefined}
+              variant="small"
+              color="gray"
+              className="font-thin text-sm"
+            >
+              {title}
+            </Typography>
+
+            {routes.map(({ routeTitle, routePath, routeIcon }) => {
+              const IconComponent = routeIcon;
+
+              return (
+                <div className="pl-3 flex flex-col my-5">
+                  <Link
+                    key={routeTitle}
+                    href={routePath}
+                    className="flex place-items-center gap-3"
+                  >
+                    <IconComponent size={20} color="#504965" />
+                    <Typography
+                      placeholder={undefined}
+                      variant="small"
+                      className="text-[#504965]"
+                    >
+                      {routeTitle}
+                    </Typography>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        ))}
       </div>
     </aside>
   );
