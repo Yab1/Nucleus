9e02f44e24a0dc9974c6fefa7f3038f0568367d6
Yab1 <yabilisanu@gmail.com>
2024-01-13 21:15:16 +0300

    Refactor Redux hooks for modularity
    Remove specific redux hooks for increased modularity and flexibility in
    hook usage. This allows for better abstraction and reusability in the
    codebase. Fixes #123.

--- hooks/reduxHooks.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
-import { RootState, AppDispatch } from "@/redux/store";
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
