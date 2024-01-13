0052af25b3eacf44ba1d4c8842b1b7de1117d694
Yab1 <yabilisanu@gmail.com>
2024-01-13 21:13:32 +0300

    Refactor Notifications component to dynamically adjust icon color
    Use useSelector hook to toggle icon color based on dark mode and remove
    redundant class in Badge component. Improves user experience in dark
    mode.

--- components/widgets/Notifications.tsx
+++ components/widgets/Notifications.tsx
@@ -1,17 +1,21 @@
 "use client";
 
+import { RootState } from "@/redux/store";
 import { Badge, IconButton } from "@material-tailwind/react";
 import { GoBell } from "react-icons/go";
+import { useSelector } from "react-redux";
 
 export default function Notifications() {
+  const darkMode = useSelector((state: RootState) => state.ui.darkMode);
+
   return (
-    <Badge content="5" color="blue" withBorder>
+    <Badge content="5" color="blue" withBorder className="border-none">
       <IconButton
         placeholder={undefined}
         color="white"
-        className="rounded-lg border border-grayish-blue w-9 h-9"
+        className="rounded-lg border border-light-primary w-9 h-9 dark:border-dark-primary dark:bg-dark-tertiary dark:shadow-gray-900 dark:hover:shadow-gray-800"
       >
-        <GoBell size={19} color="black" />
+        <GoBell size={19} color={darkMode ? "#eaeaea" : "black"} />
       </IconButton>
     </Badge>
   );
