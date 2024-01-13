497dd43bce85175cb6629af5ac5779d1d1f65c19
Yab1 <yabilisanu@gmail.com>
2024-01-13 20:03:35 +0300

    Refactor Sidebar component for dark mode support
    Introduces dark mode support in the Sidebar component by utilizing the
    state from Redux, ensuring proper icon and text colors. This update
    enhances the user experience by making the sidebar suitable for both
    light and dark themes. No associated issues.

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -5,13 +5,17 @@ import Link from "next/link";
 import { Awsomebar } from "../widgets";
 import { primaryRoutes } from "@/routes";
 import { Typography } from "@material-tailwind/react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 export default function Sidebar() {
+  const darkMode = useSelector((state: RootState) => state.ui.darkMode);
+
   return (
-    <aside className="w-1/6 border-r border-grayish-blue">
+    <aside className="w-1/6 border-r border-light-primary dark:bg-dark-primary dark:border-dark-primary">
       <Link
         href="/"
-        className="flex place-items-center gap-2 text-black font-semibold text-lg border-b border-grayish-blue h-20 pl-5"
+        className="flex place-items-center gap-2 text-black font-semibold text-lg border-b border-light-primary h-20 pl-5 dark:border-gunmetal dark:text-white bg-danger-"
       >
         <Image src="/logo.png" width={40} height={40} alt="Nucleus Logo" />
         Nucleus
@@ -21,39 +25,40 @@ export default function Sidebar() {
         <Awsomebar />
       </div>
 
-      <nav className="flex flex-col ml-6 gap-5">
+      <nav className="flex flex-col ml-6 gap-7">
         {primaryRoutes.map(({ title, routes }) => (
-          <div key={title}>
+          <div key={title} className="flex flex-col gap-3">
             <Typography
               placeholder={undefined}
-              variant="small"
-              color="gray"
-              className="font-thin text-sm"
+              className="font-thin text-[10px] !text-dark-tertiary uppercase"
             >
               {title}
             </Typography>
+            <div className="flex flex-col gap-5">
+              {routes.map(({ routeTitle, routePath, routeIcon }) => {
+                const IconComponent = routeIcon;
 
-            {routes.map(({ routeTitle, routePath, routeIcon }) => {
-              const IconComponent = routeIcon;
-
-              return (
-                <div key={routeTitle} className="pl-3 flex flex-col my-5">
+                return (
                   <Link
+                    key={routeTitle}
                     href={routePath}
                     className="flex place-items-center gap-3"
                   >
-                    <IconComponent size={20} color="#504965" />
+                    <IconComponent
+                      size={21}
+                      color={darkMode ? "#d2d2d2" : "#4d4764"}
+                    />
                     <Typography
                       placeholder={undefined}
                       variant="small"
-                      className="text-[#504965]"
+                      className="text-light-secondary dark:text-dark-secondary"
                     >
                       {routeTitle}
                     </Typography>
                   </Link>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         ))}
       </nav>
