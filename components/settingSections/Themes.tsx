487fa47d84ba065e3fda4c79d6b065ec1eb7e8ca
Yab1 <yabilisanu@gmail.com>
2024-01-15 21:34:06 +0300

    Refactor theme illustrations and buttons
    Utilize a function to dynamically generate theme illustration source
    URLs and replace Card components with button elements to enhance code
    consistency and maintainability. This streamlines the process for
    managing theme illustrations. Closes #123.

--- components/settingSections/Themes.tsx
+++ components/settingSections/Themes.tsx
@@ -24,52 +24,57 @@ export default function Themes() {
     },
   ];
 
+  const getThemeIllustrationSrc = (value: boolean) => {
+    return value
+      ? "/dark-theme-illustration.png"
+      : "/light-theme-illustration.png";
+  };
+
   return (
     <section id="themes">
       <Typography
         placeholder={undefined}
         variant="h5"
-        color="black"
-        className="mb-2"
+        className="text-light-primary text-lg mb-2  dark:text-white"
       >
         Select Theme
       </Typography>
-      <Typography placeholder={undefined} variant="small" color="gray">
+      <Typography
+        placeholder={undefined}
+        variant="small"
+        className="text-light-tertiary"
+      >
         Customize your workspace,make it more enjoyable and comfortable to work!
       </Typography>
 
       <div className="grid grid-cols-3 gap-5 my-8">
         {themes.map(({ title, value }) => (
-          <Button
-            placeholder={undefined}
-            className="p-0 bg-transparent rounded-2xl border border-light-primary shadow-sm"
+          <button
+            className="p-0 rounded-lg border border-light-primary dark:border-dark-primary shadow-sm hover:shadow-md overflow-hidden"
             onClick={() => dispatch(toggleDarkMode(value))}
           >
-            <Card placeholder={undefined}>
-              <CardBody placeholder={undefined} className="p-0 text-black">
-                <div className="bg-border-light-primary w-full h-40 rounded-xl rounded-es-none rounded-ee-none grid place-content-center">
-                  <Image
-                    src={
-                      value
-                        ? "/dark-theme-illustration.png"
-                        : "/light-theme-illustration.png"
-                    }
-                    alt="illustration of a dasboard"
-                    width={200}
-                    height={200}
-                  />
-                </div>
-                <div className="flex items-center gap-3 py-3 px-5 normal-case font-normal text-base">
-                  <div className="w-3 aspect-square ring-2 ring-blue-900 rounded-full"></div>
-                  {title}
-                </div>
-              </CardBody>
-            </Card>
-          </Button>
+            <div className="bg-light-gray dark:bg-charcoal-gray h-40 grid place-content-center">
+              <Image
+                src={getThemeIllustrationSrc(value)}
+                alt="illustration of a dashboard"
+                width={170}
+                height={200}
+              />
+            </div>
+
+            <Typography
+              placeholder={undefined}
+              variant="small"
+              className="text-light-primary dark:text-white normal-case font-normal flex px-5 py-3 bg-white dark:bg-dark-primary items-center gap-3"
+            >
+              <span className="w-3 h-3 ring-2 ring-blue-900 rounded-full"></span>
+              {title}
+            </Typography>
+          </button>
         ))}
       </div>
 
-      <div className="border border-light-primary"></div>
+      <div className="border-t border-light-primary dark:border-dark-primary"></div>
     </section>
   );
 }
