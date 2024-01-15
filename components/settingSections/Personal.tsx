a3d813abdae5805546a94fa63a5b3bdc6984870a
Yab1 <yabilisanu@gmail.com>
2024-01-15 21:34:16 +0300

    Refactor Personal section typography for improved styling
    Consolidate typography styling for enhanced readability and visual
    appeal. Improve consistency and user experience by updating the text
    styling of the Personal section. No associated issues.

--- components/settingSections/Personal.tsx
+++ components/settingSections/Personal.tsx
@@ -5,10 +5,18 @@ import { Typography } from "@material-tailwind/react";
 export default function Personal() {
   return (
     <section id="personal">
-      <Typography placeholder={undefined} variant="h5" color="black">
+      <Typography
+        placeholder={undefined}
+        variant="h5"
+        className="text-light-primary text-lg mb-2  dark:text-white"
+      >
         Notifications
       </Typography>
-      <Typography placeholder={undefined} variant="small" color="gray">
+      <Typography
+        placeholder={undefined}
+        variant="small"
+        className="text-light-tertiary"
+      >
         Stay informed and customize your notification preferences to suit your
         workflow.
       </Typography>
