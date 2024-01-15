f020ccdf53e3197ab40035aa6b83077bde323b16
Yab1 <yabilisanu@gmail.com>
2024-01-15 21:34:24 +0300

    Refactor typography styling for socials section
    Improve readability and visual consistency by updating typography
    styling in the Socials section for improved communication and
    productivity. No associated issues.

--- components/settingSections/Socials.tsx
+++ components/settingSections/Socials.tsx
@@ -5,10 +5,18 @@ import { Typography } from "@material-tailwind/react";
 export default function Socials() {
   return (
     <section id="socials">
-      <Typography placeholder={undefined} variant="h5" color="black">
+      <Typography
+        placeholder={undefined}
+        variant="h5"
+        className="text-light-primary text-lg mb-2  dark:text-white"
+      >
         My Socials
       </Typography>
-      <Typography placeholder={undefined} variant="small" color="gray">
+      <Typography
+        placeholder={undefined}
+        variant="small"
+        className="text-light-tertiary"
+      >
         Improve communication and productivity by utilizing social media share
       </Typography>
     </section>
