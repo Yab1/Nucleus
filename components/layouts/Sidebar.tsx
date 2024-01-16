8bfcd57b35bf44049265bd8728ac3f2f12d8ce5b
Yab1 <yabilisanu@gmail.com>
2024-01-16 11:08:48 +0300

    Refactor Sidebar to Highlight Active Route
    Adjust the Sidebar component to visually distinguish the active route
    using the pathname obtained from the next/navigation package. This
    enhances user navigation and interaction, improving the overall user
    experience. #143

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -7,9 +7,11 @@ import { primaryRoutes } from "@/routes";
 import { Typography } from "@material-tailwind/react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
   const darkMode = useSelector((state: RootState) => state.ui.darkMode);
+  const pathname = usePathname();
 
   return (
     <aside className="w-1/6 border-r border-light-primary dark:bg-dark-primary dark:border-dark-primary">
@@ -25,33 +27,45 @@ export default function Sidebar() {
         <Awsomebar />
       </div>
 
-      <nav className="flex flex-col ml-6 gap-7">
+      <nav className="flex flex-col mx-3 gap-5">
         {primaryRoutes.map(({ title, routes }) => (
           <div key={title} className="flex flex-col gap-3">
             <Typography
               placeholder={undefined}
-              className="font-thin text-[10px] !text-dark-tertiary uppercase"
+              className="font-thin text-[10px] !text-dark-tertiary uppercase ml-3"
             >
               {title}
             </Typography>
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-3">
               {routes.map(({ routeTitle, routePath, routeIcon }) => {
                 const IconComponent = routeIcon;
+                const active = pathname === routePath ? true : false;
 
                 return (
                   <Link
                     key={routeTitle}
                     href={routePath}
-                    className="flex place-items-center gap-3"
+                    className={
+                      "flex place-items-center gap-3 " +
+                      (active
+                        ? "bg-light-secondary py-2 rounded-md pl-3 dark:bg-dark-secondary"
+                        : "ml-3")
+                    }
                   >
                     <IconComponent
-                      size={21}
-                      color={darkMode ? "#d2d2d2" : "#4d4764"}
+                      size={20}
+                      color={
+                        active ? "#552cf9" : darkMode ? "#d2d2d2" : "#4d4764"
+                      }
                     />
                     <Typography
                       placeholder={undefined}
                       variant="small"
-                      className="text-light-secondary dark:text-dark-secondary"
+                      className={
+                        active
+                          ? "text-electric-purple"
+                          : "text-light-secondary dark:text-dark-secondary"
+                      }
                     >
                       {routeTitle}
                     </Typography>
